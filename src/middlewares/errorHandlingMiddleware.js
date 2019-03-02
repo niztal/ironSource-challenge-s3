166edd29c4c90ee651457f8218cb8e28d04fd8c3
NTNET\nitzant <nitzan.tal@amdocs.com>
2019-03-02 19:43:05 +0200

    support get metadata

--- src/middlewares/errorHandlingMiddleware.js
+++ src/middlewares/errorHandlingMiddleware.js
@@ -1,7 +1,7 @@
 export default (err, req, res, next) => {
     if (err) {
         console.error(err);
-        res.status(err.status ? err.status : 500).send(err.message);
+        res.status(err.status ? err.status : 500).send({message: err.message});
     } else{
         next();
     }
--- src/routes/download.js
+++ src/routes/download.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.get('/', async (req, res, next) => {
     const {userId} = req;
-    const {fileName, access_token} = req.query;
+    const {fileName, metadata} = req.query;
 
     if(!fileName) {
         res.status(400).send({message: "missing file name"});
@@ -16,7 +16,11 @@ router.get('/', async (req, res, next) => {
         try{
             const file = await getFileByName(userId, fileName);
             validateFile(file);
-            res.download(path.join(path.resolve(__dirname, '../../'), 'storage', userId, file.name));
+            if (metadata) {
+                res.send(getFileMetaData(file));
+            } else {
+                res.download(path.join(path.resolve(__dirname, '../../'), 'storage', userId, file.name));
+            }
         } catch(err) {
             next(err);
         }
@@ -26,7 +30,7 @@ router.get('/', async (req, res, next) => {
 router.get('/:fileId', async (req, res, next) => {
     const {userId} = req;
     const {fileId} = req.params;
-    const {access_token} = req.query;
+    const {access_token, metadata} = req.query;
 
     if(!fileId) {
         res.status(400).send({message: "missing file id"});
@@ -37,11 +41,31 @@ router.get('/:fileId', async (req, res, next) => {
         try{
             const file = await getFileById(userId, fileId);
             validateFile(file, access_token);
-            res.download(path.join(path.resolve(__dirname, '../../'), 'storage', userId, file.name));
+            if (metadata) {
+                res.send(getFileMetaData(file));
+            } else {
+                res.download(path.join(path.resolve(__dirname, '../../'), 'storage', userId, file.name));
+            }
         } catch(err) {
             next(err);
         }
     }
 });
+
+
+const getFileMetaData = (file) => {
+    const metadata =  {
+            name: file.name,
+            size: file.size,
+            createdAt: new Date(file.createdAt)
+    };
+    if (file.updatedAt) {
+        metadata.updatedAt = new Date(file.updatedAt)
+    }
+    if (file.deletedAt) {
+        metadata.deletedAt = new Date(file.deletedAt)
+    }
+    return metadata;
+}
 
 export default router;
